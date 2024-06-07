48a9191141ca3fe2690faf4e87a92cc7153a383d
MandresyIsaia <rabenjamandresy@gmail.com>
2024-06-07 06:46:03 +0300

    sprint4

--- Framework/FrontController.java
+++ Framework/FrontController.java
@@ -1,111 +1,47 @@
 package controller;
 
-import util.*;
+import util.Util;
 import java.util.List;
-import java.util.Map;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Method;
 import annotation.*;
-import model.*;
 
 public class FrontController extends HttpServlet {
-    private HashMap<String, Mapping> map;
+    private Boolean isVoatety = false;
 
-    @Override
-    public void init() throws ServletException {
-        try {
-            String packageName = this.getInitParameter("package_name");
-            map = Util.getAllClassesSelonAnnotation(packageName, ControllerAnnotation.class);
-               
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
-    }
-
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        processRequest(req, res);
+        processedRequest(req, res);
     }
 
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        processRequest(req, res);
+        processedRequest(req, res);
     }
 
-    private void processRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        res.setContentType("text/html");
+    protected void processedRequest(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/plain");
         PrintWriter out = res.getWriter();
-
-        String url = req.getRequestURI();
-        boolean urlExists = false;
-
-        for (String key : map.keySet()) {
-            if (key.equals(url)) {
-                Mapping mapping = map.get(url);
-
-                try {
-                    Class<?> c = Class.forName(mapping.getClassName());
-                    Method m = c.getDeclaredMethod(mapping.getMethodeName());
-                    Object instance = c.getDeclaredConstructor().newInstance();
-                    Object result = m.invoke(instance);
-
-                    if (result instanceof ModelView) {
-                        ModelView mv = (ModelView) result;
-                        String jspPath = mv.getUrl();
-                        ServletContext context = getServletContext();
-                        String realPath = context.getRealPath(jspPath);
-
-                        if (realPath == null || !new File(realPath).exists()) {
-                            throw new ServletException("La page JSP " + jspPath + " n'existe pas.");
-                        }
-
-                        HashMap<String, Object> data = mv.getData();
-                        for (String keyData : data.keySet()) {
-                            req.setAttribute(keyData, data.get(keyData));
-                        }
-
-                        RequestDispatcher dispatch = req.getRequestDispatcher(jspPath);
-                        dispatch.forward(req, res);
-                    } else if (result instanceof String) {
-                        out.println(result.toString());
-                    } else {
-                        throw new ServletException("Type de retour inconnu : " + result.getClass().getName());
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    //out.println("<scrip>alert('Erreur: " + escapeJavaScript(e.getMessage()) + "')</script>");
-                    out.println("Erreur: "+e.getMessage());
-                }
-
-                urlExists = true;
-                break;
+        out.println("Tongasoa ato am FrontController");
+        out.println("Votre url : " + req.getRequestURL().toString());
+        List<String> controllerNames = new ArrayList<>();
+        if (!isVoatety) {
+            try {
+                String package_name = this.getInitParameter("package_name");
+                controllerNames = Util.getAllClassesSelonAnnotation(package_name,ControllerAnnotation.class);
+                isVoatety = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                out.println("Erreur lors du scan du package : " + e.getMessage());
             }
         }
 
-        if (!urlExists) {
-            out.println("Aucune methode n\\'est associee a l\\'url : " + url);
+        out.println("Liste des contrôleurs : ");
+        for (String controllerName : controllerNames) {
+            out.println(controllerName);
         }
     }
-
-    /*private String escapeJavaScript(String message) {
-        if (message == null) {
-            return "";
-        }
-        return message.replace("\\", "\\\\")
-                      .replace("'", "\\'")
-                      .replace("\"", "\\\"")
-                      .replace("\r", "\\r")
-                      .replace("\n", "\\n");
-    }*/
 }
