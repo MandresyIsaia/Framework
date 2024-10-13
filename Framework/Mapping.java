78e672ab1efea8a96f09509ab743baffe6b669f6
MandresyIsaia <rabenjamandresy@gmail.com>
2024-10-13 15:24:55 +0300

    sprint9

--- Framework/Mapping.java
+++ Framework/Mapping.java
@@ -1,20 +1,23 @@
-package utils;
+package util;
 
 public class Mapping {
-    String methodName;
     String className;
-
-    public String getMethodName() {
-        return methodName;
+    String methodeName;
+    public Mapping(String className, String methodeName) {
+        this.className = className;
+        this.methodeName = methodeName;
     }
-
     public String getClassName() {
         return className;
     }
-
-    public Mapping(String methodName, String className) {
-        this.methodName = methodName;
+    public void setClassName(String className) {
         this.className = className;
     }
-
+    public String getMethodeName() {
+        return methodeName;
+    }
+    public void setMethodeName(String methodeName) {
+        this.methodeName = methodeName;
+    }
+    
 }
